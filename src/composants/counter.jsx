296f3c53d4629195274c91fd536534d9f6769245
malik <malik.h@webdevpro.net>
2019-10-16 16:46:20 +0200

    rendre la className dynamique en fonction de la valeur de this.state.valeur

--- src/composants/counter.jsx
+++ src/composants/counter.jsx
@@ -4,7 +4,7 @@ class Counter extends Component {
   //cc
   state = {
     // c'est ici qu'il faudra stocker toutes les valeurs du composant
-    valeur: 0,
+    valeur: 10,
     titre: "Mon Compteur !"
   };
 
@@ -17,12 +17,20 @@ class Counter extends Component {
     return (
       <React.Fragment>
         <h1 style={this.style}>{this.state.titre}</h1>
-        <span className="badge badge-light m-2">{this.state.valeur}</span>
+        <span className={this.generateClass()}>{this.state.valeur}</span>
         <button style={{ color: "blue", fontWeight: "lighter" }}>
           Ajouter
         </button>
       </React.Fragment>
     );
+  }
+
+  generateClass() {
+    if (this.state.valeur == 0) {
+      return "badge badge-light m-2";
+    } else {
+      return "badge badge-danger m-2";
+    }
   }
 }
 
